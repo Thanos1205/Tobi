e6dd96e07f9b271741b15202aad4765aef3bfe4f
Thanos1205 <113518730+Thanos1205@users.noreply.github.com>
2022-09-14 18:58:49 +0200

    Update Tobi_password.py

--- Tobi_password.py
+++ Tobi_password.py
@@ -47,7 +47,19 @@
 #
 #
 #
-input("\nTo unlock the safe you need a 4-digit password. Here is a hint: 'https://lichess.org/isisissi'")
+#
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+image = mpimg.imread('pic.png')
+plt.imshow(image)
+
+input("\nThanks for helping me Mr. ... ... Tomatoehead? <enter>")
+input('\nAnywhooo. To unlock my online wallet you need a 4-digit password. <enter>')
+input("I forgot it and the hint you get after entering a wrong one couldn't help me. <enter>")
+input("Maybe you can do better. <enter>")
+input("\nPassword: ")
+input("\n- invalid password -\nHint: 'https://lichess.org/isisissi'")
+input("\nStrange URL. What could this mean? <enter>\n")
 
 while True:
     if input("What does the variable 'i' stand for in the URL? ") == 'integer':
@@ -55,105 +67,94 @@ while True:
 while True:
     if input("What does the variable 's' stand for? ") == 'string':
         break
+input('\nOf course. You so clevaaaaaaaa. You make so good choice. You take advantage of me being stuuuupid, you so clevaaaaaaa. You like fukn stupid people? <enter>')
+input("I know what you doin. You walk by me GitHub and think 'Oh, there is nice stupid boy. I think I go fuck hiiiiiim. You stupid-boy Fucker, you!' <enter>")
+input('\nMoving on, now try to figure out the correct URL by using both textfiles... <enter>')
+input("\nAfter reading the first text file, I couldn't find any 4-digit numbers. Can you? <enter>")
+input("So many letters in the second text file, but which 4 letters do I need... <enter>\n")
 
-input('Now try to figure out the correct URL by using both textfiles...')
-input("\nAfter reading the first text file, I couldn't find any 4-digit numbers. Can you?")
-input("So many letters in the second text file, but which 4 letters do I need...")
 while True:
     if input('Password: ') == 'Qc6#':
         input('\nYes. You unlocked the safe. Lets see what we can find in here.\n\nCity: 54.133333°, 12.3°')
+        input("Oh yeah I member. I don't trust the government, thats why I digged a hole to hide all my money there and these are the coordinates.")
+        input('But I also forgot how to read coordinates. Oh Toooooooooooooooooooobi, please help.')
         break
     else:
-        print('Wrong')
-input('\nCoordinates! Interesting...')
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+        print('- invalid password -')
+
+while True:
+    if input('\nWhere is my money? ') == 'Poppendorf':
+        print('Yes. Now I know where I should go.')
+        print("You know what you should do meanwhile... You should try to get from wikipedias 'Poppenhausen' to 'Elisabeth II.'")
+        break
+    else:
+        print('You sure?')
+
+input('Did you do it? (y/n) ')
+input("Nice. I also found my wallet but figured I didn't have any money. <enter>")
+input("Rip Elisabeth II. I hope you won't die. Especially not in Bloons. <enter>")
+input('Theeeeeeeeeeeeeeeeeeeerefore, I need to test your gaming skills etc. <enter>\n')
+
+input('\nQuestion 1:\tWhat defeats R28 on Cubism Chimps?\n\t\ta) 2-5-0 Tack Shooter\n\t\tb) 0-2-5 Sniper\n\t\tc) 5-0-2 Sub\n\t\td) 2-0-5 Dartling')
+input('\nQuestion 2:\thttps://lichess.org/training/rtCDf')
+input('\nQuestion 3:\t--- Chess ---')
+input("\nQuestion 4:\tWhich 'Game of Thrones'-Character appears in more episodes than any other?")
+input('\nQuestion 5:\tWelches Level in Titan Quest hat der Charakter am Ende von Hades auf "Normal" erreicht, mit dem der Speedrun Weltrekord aufgestellt wurde.\n\t\ta) 13\n\t\tb) 19\n\t\tc) 25\n\t\td) 31')
+input('\nQuestion 6:\tInformatik <enter>')
+plt.show()
+input('\nQuestion 7:\t--- Politics ---')
+input('\nQuestion 8:\tWas davon trifft auf die meisten in Deutschland studierenden Menschen zu (Stand 2021)?\n\t\ta) Sie sind im deutschen Schachbund\n\t\tb) Sie leben in Darmstadt\n\t\tc) Sie studieren Informatik\n\t\td) Sie studieren Psychologie')
+input('\nQuestion 9:\tWelche dieser Städte grenzt nicht direkt an Langen?\n\t\ta) Dietzenbach\n\t\tb) Neu-Isenburg\n\t\tc) Mörfelden-Walldorf\n\t\td) Darmstadt')
+input('\nQuestion 10:\t--- Personal ---')
+
+input('\nReally nice done Mr. Tomatoehead. <enter>')
+input('After proving your skills, you can now use them by playing 1 Chess960 game against Classy_Master & DerGegner each. Come back afterwards. <enter>\n')
+input('Very nicely played my friend. <enter>')
+
+input("I also heard you are a good programmer. Prove it. Which code is flawless and what's the output?")
+input('''
+1   why, are, you, gae = 'nigga', 'what', 'the', 'fuck?'
+2   this, nigga, better, be, kläpn = 1, '1', 3, '4', 2
+3   i = [this, nigga, better, be , kläpn]\n''')
+
+input('''--- Code 1 ---
+try:
+    for x in i:
+        if x.isdigit():
+            print(why)
+except:
+    print(this+nigga)\n\n''')
+
+input('''--- Code 2 ---
+x,z,y,xy,zx,xz=2,4,3,10,8,5
+for list in i:
+    if z and x<xy and zx>xz:
+        print('if?') if list == i[1] else print('what')
+        z+=y
+        xy-=x*2
+    else:
+        print(kläpn)
+        break\n\n''')
+
+input('''--- Code 3 ---
+for x in i:
+    if x.isdigit():
+        print('x')\n\n''')
+
+while True:
+    if input('Correct Code: ') == '2':
+        print('Congratulations!')
+        image = mpimg.imread('pic2.png')
+        plt.imshow(image)
+        plt.show()
+        break
+    else:
+        pass
+
+#
+#
+#
+
 """
 Solution:
 
